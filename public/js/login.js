ce9e2a8dde0cbad299c2a60b79a076bbe9937c3e
HarryFelty <harryfelty@gmail.com>
2023-09-07 10:28:17 -0400

    adding homepage handlebar

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,13 @@
 const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
-  const username = document.querySelector('#username-login').value.trim();
+  const user_name = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (username && password) {
-    // TODO: Add a comment describing the functionality of this expression
+  if (user_name && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ user_name, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
