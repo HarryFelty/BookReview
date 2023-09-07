176dd551f71ff0ccfd6d9a37fd9607d65d5c5219
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-09-07 10:11:49 -0400

    login styling

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
 const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
+  
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
+  
   const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    // TODO: Add a comment describing the functionality of this expression
+   
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
